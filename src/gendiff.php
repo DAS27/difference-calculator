48245d700f27cf4c1aa3a701d36f70213f4f7174
Damir <sarkulindamir@gmail.com>
2019-11-29 17:30:53 +0500

    first commit

---
+++ src/gendiff.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GenDiff\GenDiff;
+
+$doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff (-v|--version)
+
+Options:
+  -h --help                     Show this screen
+  -v --version                  Show version
+
+DOC;
+
+print_r($doc);

a2b12477b23840926ea696747c2c49d305c52d0e
Damir <sarkulindamir@gmail.com>
2019-12-02 11:18:25 +0500

    fix v.1

--- src/gendiff.php
+++ src/gendiff.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace GenDiff\GenDiff;
-
 $doc = <<<DOC
 Generate diff
 
@@ -15,4 +13,7 @@ Options:
 
 DOC;
 
-print_r($doc);
+require('../vendor/docopt/docopt/src/docopt.php');
+$args = Docopt::handle($doc, array('version' => 'Generate diff 2.0'));
+foreach ($args as $k => $v)
+    echo $k . ': ' . json_encode($v) . PHP_EOL;

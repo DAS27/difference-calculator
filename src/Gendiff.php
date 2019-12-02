d89675e46ddb28fe3f22dda14aad61a1c407144a
Damir <sarkulindamir@gmail.com>
2019-12-02 17:01:50 +0500

    fix code and add Travis in Readme

---
+++ src/Gendiff.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace GenDiff;
+
+function run()
+{
+    $doc = <<<DOC
+    Generate diff
+
+    Usage:
+    gendiff (-h|--help)
+    gendiff (-v|--version)
+
+    Options:
+    -h --help                     Show this screen
+    -v --version                  Show version
+
+DOC;
+
+    $args = \Docopt::handle($doc, array('version' => 'Generate diff 2.0'));
+    foreach ($args as $k => $v) {
+        echo $k . ': ' . json_encode($v) . PHP_EOL;
+    }
+}

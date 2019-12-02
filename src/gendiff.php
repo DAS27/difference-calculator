f2fb4a8cf4817513c2d98deb492c5a9bb01b43ba
Damir <sarkulindamir@gmail.com>
2019-12-02 16:26:25 +0500

    fix v.2

--- src/gendiff.php
+++ src/gendiff.php
@@ -1,19 +1,25 @@
 <?php
 
+namespace GenDiff;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function run()
+{
 $doc = <<<DOC
 Generate diff
 
 Usage:
-  gendiff (-h|--help)
-  gendiff (-v|--version)
+gendiff (-h|--help)
+gendiff (-v|--version)
 
 Options:
-  -h --help                     Show this screen
-  -v --version                  Show version
+-h --help                     Show this screen
+-v --version                  Show version
 
 DOC;
 
-require('../vendor/docopt/docopt/src/docopt.php');
-$args = Docopt::handle($doc, array('version' => 'Generate diff 2.0'));
-foreach ($args as $k => $v)
-    echo $k . ': ' . json_encode($v) . PHP_EOL;
+  $args = \Docopt::handle($doc, array('version' => 'Generate diff 2.0'));
+  foreach ($args as $k => $v)
+  echo $k . ': ' . json_encode($v) . PHP_EOL;
+}

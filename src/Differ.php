e09f85c6fd388305bbb6d7f75c807e2e13a6af8e
Damir Sarkulin <sarkulindamir@gmail.com>
2020-03-05 16:44:09 +0500

    fix names for variable

--- src/Differ.php
+++ src/Differ.php
@@ -6,27 +6,30 @@ use function Funct\Collection\union;
 
 function genDiff($pathToFile1, $pathToFile2)
 {
-    $file1 = json_decode(file_get_contents($pathToFile1), true);
-    $file2 = json_decode(file_get_contents($pathToFile2), true);
+    $dataFromPath1 = file_get_contents($pathToFile1);
+    $dataFromPath2 = file_get_contents($pathToFile2);
 
-    $keys1 = array_keys($file1);
-    $keys2 = array_keys($file2);
+    $dataToArray1 = json_decode($dataFromPath1, true);
+    $dataToArray2 = json_decode($dataFromPath2, true);
+
+    $keys1 = array_keys($dataToArray1);
+    $keys2 = array_keys($dataToArray2);
     $keys = union($keys1, $keys2);
 
     return array_reduce(
         $keys,
-        function ($acc, $key) use ($file1, $file2) {
-            if (array_key_exists($key, $file1) && array_key_exists($key, $file2)) {
-                if ($file1[$key] === $file2[$key]) {
-                    $result = "{$key} {$file2[$key]}\n";
+        function ($acc, $key) use ($dataToArray1, $dataToArray2) {
+            if (array_key_exists($key, $dataToArray1) && array_key_exists($key, $dataToArray2)) {
+                if ($dataToArray1[$key] === $dataToArray2[$key]) {
+                    $result = "{$key}: {$dataToArray2[$key]}\n";
                 } else {
-                    $result = "+ {$key} {$file2[$key]}\n";
-                    $result .= "- {$key} {$file1[$key]}\n";
+                    $result = "+ {$key}: {$dataToArray2[$key]}\n";
+                    $result .= "- {$key}: {$dataToArray1[$key]}\n";
                 }
-            } elseif (array_key_exists($key, $file1) && !array_key_exists($key, $file2)) {
-                $result = "- {$key} {$file1[$key]}\n";
+            } elseif (array_key_exists($key, $dataToArray1) && !array_key_exists($key, $dataToArray2)) {
+                $result = "- {$key}: {$dataToArray1[$key]}\n";
             } else {
-                $result = "+ {$key} {$file2[$key]}\n";
+                $result = "+ {$key}: true\n";
             }
             $acc .= $result;
             return $acc;

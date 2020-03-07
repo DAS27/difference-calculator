02f177da02504d6ad43224acddcdc2870d16a729
Damir <sarkulindamir@gmail.com>
2020-03-07 18:02:11 +0500

    edit path to file in test

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,19 +8,18 @@ use function Differ\genDiff;
 class DifferTest extends TestCase
 {
     protected $data;
-    protected $pathToFile1;
-    protected $pathToFile2;
 
     protected function setUp(): void
     {
         $this->data = "host: hexlet.io\n+ timeout: 20\n- timeout: 50\n- proxy: 123.234.53.22\n+ verbose: true\n";
-        $this->pathToFile1 = __DIR__ . '/fixtures/before.json';
-        $this->pathToFile2 = __DIR__ . '/fixtures/after.json';
     }
 
     public function testGenDiff()
     {
-        $actual = genDiff($this->pathToFile1, $this->pathToFile2);
+        $pathToFile1 = __DIR__ . '/fixtures/before.json';
+        $pathToFile2 = __DIR__ . '/fixtures/after.json';
+
+        $actual = genDiff($pathToFile1, $pathToFile2);
         $this->assertEquals($this->data, $actual);
     }
 }

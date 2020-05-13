e819dbe90e4049f309ce04d1536f90c7ac1789a2
Damir Sarkulin <sarkulindamir@gmail.com>
2020-05-13 09:41:50 +0500

    finished step 6

--- src/ast.php
+++ src/ast.php
@@ -4,37 +4,16 @@ namespace Ast;
 
 use function Funct\Collection\union;
 
-function mknode(string $key, string $type, array $children = [], $oldValue , $newValue) {
+function mknode(string $key, string $type, $oldValue, $newValue, array $children = [])
+{
     return [
         'key' => $key,
         'type' => $type,
-        'children' => $children,
         'oldValue' => $oldValue,
-        'newValue' => $newValue
+        'newValue' => $newValue,
+        'children' => $children,
     ];
 }
-/*
-function buildDiff($arr1, $arr2)
-{
-    $keys = union(array_keys($arr1), array_keys($arr2));
-    return array_map(function ($key) use ($arr1, $arr2) {
-        if (!isset($arr1[$key])) {
-            return mknode($key, 'added', [], null, $arr2[$key]);
-        }
-        if (!isset($arr2[$key])) {
-            return mknode($key, 'deleted', [] , $arr1[$key], null);
-        }
-        if (is_array($arr1[$key]) && is_array($arr2[$key])) {
-            return mknode($key, 'nested', buildDiff($arr1[$key], $arr2[$key]), null, null);
-        }
-        if ($arr1[$key] !== $arr2[$key]) {
-            return mknode($key, 'edited', [], $arr1[$key], $arr2[$key]);
-        }
-
-        return mknode($key, 'unchanged', [], $arr1[$key], null);
-    }, $keys);
-}
-*/
 
 function buildDiff($data1, $data2)
 {
@@ -42,25 +21,25 @@ function buildDiff($data1, $data2)
     return array_reduce($keys, function ($acc, $key) use ($data1, $data2) {
         if (array_key_exists($key, $data1) && array_key_exists($key, $data2)) {
             if ($data1[$key] == $data2[$key]) {
-                $acc[] = mknode($key, 'unchanged', [], $data1[$key], null);
+                $acc[] = mknode($key, 'unchanged', $data1[$key], null);
                 return  $acc;
             }
         }
         if (!array_key_exists($key, $data1) && array_key_exists($key, $data2)) {
-            $acc[] = mknode($key, 'added', [],null, $data2[$key]);
+            $acc[] = mknode($key, 'added', null, $data2[$key]);
             return $acc;
         }
         if (array_key_exists($key, $data1) && !array_key_exists($key, $data2)) {
-            $acc[] = mknode($key, 'deleted', [], $data1[$key], null);
+            $acc[] = mknode($key, 'deleted', $data1[$key], null);
             return $acc;
         }
         if (is_array($data1[$key]) && is_array($data2[$key])) {
-            $acc[] = mknode($key, 'nested', buildDiff($data1[$key], $data2[$key]), null, null);
+            $acc[] = mknode($key, 'nested', null, null, buildDiff($data1[$key], $data2[$key]));
             return  $acc;
         }
         if (array_key_exists($key, $data1) && array_key_exists($key, $data2)) {
             if ($data1[$key] !== $data2[$key]) {
-                $acc[] = mknode($key, 'edited', [], $data1[$key], $data2[$key]);
+                $acc[] = mknode($key, 'edited', $data1[$key], $data2[$key]);
                 return $acc;
             }
         }
@@ -68,47 +47,3 @@ function buildDiff($data1, $data2)
         return $acc;
     }, []);
 }
-
-/*
-function buildDiff($arr1, $arr2)
-{
-    $keys = union(array_keys($arr1), array_keys($arr2));
-    return array_map(function ($key) use ($arr1, $arr2) {
-        if (!isset($arr1[$key])) {
-            return [
-                'name' => $key,
-                'type' => 'added',
-                'newValue' => $arr2[$key]
-            ];
-        }
-        if (!isset($arr2[$key])) {
-            return [
-                'name' => $key,
-                'type' => 'deleted',
-                'oldValue' => $arr1[$key]
-            ];
-        }
-        if (is_array($arr1[$key]) && is_array($arr2[$key])) {
-            return [
-                'name' => $key,
-                'type' => 'nested',
-                'children' => buildDiff($arr1[$key], $arr2[$key])
-            ];
-        }
-        if ($arr1[$key] !== $arr2[$key]) {
-            return [
-                'name' => $key,
-                'type' => 'edited',
-                'oldValue' => $arr1[$key],
-                'newValue' => $arr2[$key]
-            ];
-        }
-
-        return [
-            'name' => $key,
-            'type' => 'unchanged',
-            'oldValue' => $arr1[$key]
-        ];
-    }, $keys);
-}
-*/
--- src/render.php
+++ src/render.php
@@ -4,58 +4,70 @@ namespace Renderer;
 
 function render($tree)
 {
-    $iter = function ($node) use (&$iter) {
-        return array_reduce($node, function ($acc, $n) use ($iter, $node) {
-//            print_r($n);
+    $operators = [
+        'plus' => '+',
+        'minus' => '-',
+        'space' => "  ",
+    ];
+    $iter = function ($node, $level = 1) use (&$iter, $operators) {
+        return array_reduce($node, function ($acc, $n) use ($operators, $iter, $level) {
             ['key' => $key, 'type' => $type, 'children' => $children] = $n;
-//            $indent = str_repeat(' ', 4 * 1);
+            ['plus' => $plus, 'minus' => $minus, 'space' => $space] = $operators;
+            $indent = str_repeat(' ', 2 * $level);
             switch ($type) {
                 case 'unchanged':
-                    $acc .= "  {$key}: ";
-                    $acc .= stringify($n['oldValue']);
+                    $acc .= "{$indent}{$space}{$key}: ";
+                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
                     break;
                 case 'deleted':
-                    $acc .= "- {$key}: ";
-                    $acc .= stringify($n['oldValue']);
+                    $acc .= "{$indent}{$minus} {$key}: ";
+                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
                     break;
                 case 'added':
-                    $acc .= "+ {$key}: ";
-                    $acc .= stringify($n['newValue']);
+                    $acc .= "{$indent}{$plus} {$key}: ";
+                    $acc .= is_array($n['newValue']) ? stringify($n['newValue'], $indent) : isBool($n['newValue']);
                     break;
                 case 'edited':
-                    $acc .= "+ {$key}: ";
-                    $acc .= stringify($n['newValue']);
-                    $acc .= "- {$key}: ";
-                    $acc .= stringify($n['oldValue']);
+                    $acc .= "{$indent}{$plus} {$key}: ";
+                    $acc .= is_array($n['newValue']) ? stringify($n['newValue'], $indent) : isBool($n['newValue']);
+                    $acc .= "{$indent}{$minus} {$key}: ";
+                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
                     break;
                 case 'nested':
-                    $acc .= "  {$key}: ";
-                    $acc .= $iter($children);
-                    $acc .= "  }\n";
+                    $acc .= "{$indent}{$space}{$key}: ";
+                    $acc .= $iter($children, 3);
+                    $acc .= "{$indent}{$indent}}\n";
                     break;
             }
             return $acc;
         }, "{\n");
     };
 
-    return $iter($tree);
+    $result = $iter($tree);
+    $result .= '}';
+    return $result;
 }
 
-function stringify($n)
+function stringify($n, $indent)
 {
     $result = '';
-    $level = 0;
     if (is_array($n)) {
         $result .= "{\n";
-        $level++;
-        $indent = str_repeat(' ', 4 * $level);
         foreach ($n as $key => $value) {
-            $result .= $indent;
-            $result .= "{$key}: {$value}\n";
+            $indent2 = str_repeat(' ', 6);
+            $result .= "{$indent}{$indent2}{$key}: {$value}\n";
         }
-        $result .= "  }\n";
+        $result .= "  {$indent}}\n";
         return $result;
     }
-    $result .= "{$n}\n";
+
     return $result;
+}
+
+function isBool($value)
+{
+    if (is_bool($value)) {
+        return "true\n";
+    }
+    return "{$value}\n";
 }

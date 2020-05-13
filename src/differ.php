a94e60de08be024d37248838bb51bd2cd8c0a55d
Damir <sarkulindamir@gmail.com>
2020-05-13 10:18:02 +0500

    fix code

--- src/differ.php
+++ src/differ.php
@@ -17,6 +17,5 @@ function genDiff($pathToFile1, $pathToFile2)
     $dataToArray2 = json_decode($dataFromPath2, true);
 
     $result = buildDiff($dataToArray1, $dataToArray2);
-//    print_r(render($result));
     return render($result);
 }

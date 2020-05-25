9aa00d195977a38ead75a485b53ae64f94544333
Damir Sarkulin <sarkulindamir@gmail.com>
2020-05-26 00:14:53 +0500

    fix code

--- src/render.php
+++ src/render.php
@@ -9,33 +9,54 @@ function render($tree)
         'minus' => '-',
         'space' => "  ",
     ];
+
     $iter = function ($node, $level = 1) use (&$iter, $operators) {
         return array_reduce($node, function ($acc, $n) use ($operators, $iter, $level) {
-            ['key' => $key, 'type' => $type, 'children' => $children] = $n;
+            ['key' => $key, 'type' => $type, 'oldValue' => $oldValue, 'newValue' => $newValue] = $n;
             ['plus' => $plus, 'minus' => $minus, 'space' => $space] = $operators;
             $indent = str_repeat(' ', 2 * $level);
             switch ($type) {
                 case 'unchanged':
                     $acc .= "{$indent}{$space}{$key}: ";
-                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
+                    if (is_array($oldValue)) {
+                        $acc .= str_replace('"', null, stringify($oldValue, $indent));
+                    } else {
+                        $acc .= str_replace('"', null, isBool($oldValue));
+                    }
                     break;
                 case 'deleted':
                     $acc .= "{$indent}{$minus} {$key}: ";
-                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
+                    if (is_array($oldValue)) {
+                        $acc .= str_replace('"', null, stringify($oldValue, $indent));
+                    } else {
+                        $acc .= str_replace('"', null, isBool($oldValue));
+                    }
                     break;
                 case 'added':
                     $acc .= "{$indent}{$plus} {$key}: ";
-                    $acc .= is_array($n['newValue']) ? stringify($n['newValue'], $indent) : isBool($n['newValue']);
+                    if (is_array($newValue)) {
+                        $acc .= str_replace('"', null, stringify($newValue, $indent));
+                    } else {
+                        $acc .= str_replace('"', null, isBool($newValue));
+                    }
                     break;
                 case 'edited':
                     $acc .= "{$indent}{$plus} {$key}: ";
-                    $acc .= is_array($n['newValue']) ? stringify($n['newValue'], $indent) : isBool($n['newValue']);
+                    if (is_array($newValue)) {
+                        $acc .= str_replace('"', null, stringify($newValue, $indent));
+                    } else {
+                        $acc .= str_replace('"', null, isBool($newValue));
+                    }
                     $acc .= "{$indent}{$minus} {$key}: ";
-                    $acc .= is_array($n['oldValue']) ? stringify($n['oldValue'], $indent) : isBool($n['oldValue']);
+                    if (is_array($oldValue)) {
+                        $acc .= str_replace('"', null, stringify($oldValue, $indent));
+                    } else {
+                        $acc .= str_replace('"', null, isBool($oldValue));
+                    }
                     break;
                 case 'nested':
                     $acc .= "{$indent}{$space}{$key}: ";
-                    $acc .= $iter($children, 3);
+                    $acc .= $iter($n['children'], 3);
                     $acc .= "{$indent}{$indent}}\n";
                     break;
             }
@@ -50,12 +71,12 @@ function render($tree)
 
 function stringify($n, $indent)
 {
-    $result = '';
+    $result = "";
     if (is_array($n)) {
         $result .= "{\n";
         foreach ($n as $key => $value) {
-            $indent2 = str_repeat(' ', 6);
-            $result .= "{$indent}{$indent2}{$key}: {$value}\n";
+            $indent2 = str_repeat(" ", 6);
+            $result .= "{$indent}{$indent2}\"$key: $value\"\n";
         }
         $result .= "  {$indent}}\n";
         return $result;
@@ -67,7 +88,7 @@ function stringify($n, $indent)
 function isBool($value)
 {
     if (is_bool($value)) {
-        return "true\n";
+        return "\"true\"\n";
     }
-    return "{$value}\n";
+    return "\"$value\"\n";
 }

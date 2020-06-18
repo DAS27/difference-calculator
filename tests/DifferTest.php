17613c8e7c3bf0985f7aabf3c50365a8e709d1df
Damir Sarkulin <sarkulindamir@gmail.com>
2020-06-19 01:05:42 +0500

    added test diff yml(yaml) files

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -4,18 +4,25 @@ namespace Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 use function Differ\genDiff;
-use function Formatter\renderPlainDiff;
 
 class DifferTest extends TestCase
 {
     protected $path1 = __DIR__ . '/fixtures/before.json';
     protected $path2 = __DIR__ . '/fixtures/after.json';
+    protected $path3 = __DIR__ . '/fixtures/before.yml';
+    protected $path4 = __DIR__ . '/fixtures/after.yml';
 
-    //TODO: переделать тесты! Выводить результат в зависимости от формата.
     public function testGenDiff()
     {
         $expected = file_get_contents(__DIR__ . '/fixtures/diff.pretty');
         $actual = genDiff($this->path1, $this->path2);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenDiff2()
+    {
+        $expected = file_get_contents(__DIR__ . '/fixtures/diff.pretty');
+        $actual = genDiff($this->path3, $this->path4);
         $this->assertEquals($expected, $actual);
     }
 
@@ -26,10 +33,24 @@ class DifferTest extends TestCase
         $this->assertEquals($expected, $actual);
     }
 
+    public function testRenderPlainDiff2()
+    {
+        $expected = file_get_contents(__DIR__ . '/fixtures/diff.pretty');
+        $actual = genDiff($this->path3, $this->path4);
+        $this->assertEquals($expected, $actual);
+    }
+
     public function testRenderJsonDiff()
     {
         $expected = file_get_contents(__DIR__ . '/fixtures/diff.json');
         $actual = genDiff($this->path1, $this->path2, 'json');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testRenderJsonDiff2()
+    {
+        $expected = file_get_contents(__DIR__ . '/fixtures/diff.pretty');
+        $actual = genDiff($this->path3, $this->path4);
         $this->assertEquals($expected, $actual);
     }
 }

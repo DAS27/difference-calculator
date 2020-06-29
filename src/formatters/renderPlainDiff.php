802fc23b6a4c81965492b69272bff4c877d31263
Damir Sarkulin <sarkulindamir@gmail.com>
2020-06-30 00:49:27 +0500

    refactor renderPlainDiff function

--- src/formatters/renderPlainDiff.php
+++ src/formatters/renderPlainDiff.php
@@ -1,38 +1,8 @@
 <?php
 
 namespace Differ\Formatters;
-/*
-function renderPlainDiff($tree)
-{
-    $iter = function ($node, $ancestry = '') use (&$iter) {
-        return array_reduce($node, function ($acc, $item) use ($iter, $ancestry) {
-            ['key' => $key, 'type' => $type, 'oldValue' => $oldValue, 'newValue' => $newValue] = $item;
-            $nestedPath = trim("{$ancestry}.{$key}", '.');
-            $value = stringify($newValue);
-            switch ($type) {
-                case 'deleted':
-                    $acc .= "Property '{$nestedPath}' was removed\n";
-                    break;
-                case 'added':
-                    $acc .= "Property '{$nestedPath}' was added with value: ";
-                    $acc .= stringify($newValue);
-                    break;
-                case 'edited':
-                    $acc .= "\nProperty '{$nestedPath}' was changed. From '{$oldValue}' to '{$newValue}'\n";
-                    break;
-                case 'nested':
-                    $acc .= $iter($item['children'], $key);
-                    break;
-            }
-            return $acc;
-        }, '');
-    };
 
-    return $iter($tree);
-}
-*/
-
-use function Funct\Collection\flatten;
+use function Funct\Collection\flattenAll;
 
 function renderPlainDiff($tree)
 {
@@ -40,23 +10,29 @@ function renderPlainDiff($tree)
         return array_map(function ($item) use ($iter, $ancestry) {
             ['key' => $key, 'type' => $type, 'oldValue' => $oldValue, 'newValue' => $newValue] = $item;
             $nestedPath = trim("{$ancestry}.{$key}", '.');
+            $newValue = stringify($newValue);
             switch ($type) {
+                case 'edited':
+                    return "Property '{$nestedPath}' was changed. From '{$oldValue}' to '{$newValue}'";
+                    break;
                 case 'deleted':
                     return "Property '{$nestedPath}' was removed";
                     break;
                 case 'added':
-                    return "Property '{$nestedPath}' was added with value: ";
+                    return "Property '{$nestedPath}' was added with value: '{$newValue}'";
                     break;
+                default:
+                    return $iter($item['children'], $key);
             }
         }, $node);
     };
 
-    $flattened = flatten($iter($tree));
+    $flattened = flattenAll($iter($tree));
 
     return implode("\n", $flattened);
 }
 
 function stringify($value)
 {
-    return is_array($value) ? "'complex value'" : "'{$value}'";
+    return is_array($value) ? "complex value" : "{$value}";
 }
